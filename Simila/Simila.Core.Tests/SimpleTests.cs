a7deb3359609549f42cf52a1838545130cd824ed
Mehran <mehrandvd@yahoo.com>
2015-04-05 22:47:12 +0430

    First Unit Test

---
+++ Simila/Simila.Core.Tests/SimpleTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Simila.Core.Tests
+{
+    [TestClass]
+    public class SimpleTests
+    {
+        [TestMethod]
+        public void SimpleSimilaritiesMustWork()
+        {
+        }
+    }
+}
