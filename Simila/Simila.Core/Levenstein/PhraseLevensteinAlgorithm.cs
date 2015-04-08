9baf1de534690092ece9168fef90ec02ed8c7d44
Mehran <mehrandvd@yahoo.com>
2015-04-08 23:22:21 +0430

    Add the missing file: PhraseLevensteinAlgorithm.cs

---
+++ Simila/Simila.Core/Levenstein/PhraseLevensteinAlgorithm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Simila.Core;
+using Simila.Core.Levenstein;
+using Simila.Core.Levenstein.CostResolvers;
+
+namespace LevenshtienAlgorithm
+{
+    public class PhraseLevensteinAlgorithm : LevenshteinAlgorithm<Phrase, Word>,  ISimilarityAlgorithm
+    {
+        public PhraseLevensteinAlgorithm(): base(new WordCostResolver())
+        {
+        }
+
+        public double GetSimilarity(string left, string right)
+        {
+            return GetSimilarity((Phrase) left, right);
+        }
+    }
+}
