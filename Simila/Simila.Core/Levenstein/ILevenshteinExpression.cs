827808c74327637e309645ad78924177a5d3f5c7
Mehran <mehrandvd@yahoo.com>
2015-04-23 02:02:52 +0430

    Some code adornments on namespaces

--- Simila/Simila.Core/Levenstein/ILevenshteinExpression.cs
+++ Simila/Simila.Core/Levenstein/ILevenshteinExpression.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace LevenshtienAlgorithm
+﻿namespace Simila.Core.Levenstein
 {
     public interface ILevenshteinExpression<T>
     {
