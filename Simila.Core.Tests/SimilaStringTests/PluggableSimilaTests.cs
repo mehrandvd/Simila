6748d4037182fa43c10c884ce71872bbe0e58ae8
Mehran Davoudi <mehrandvd@yahoo.com>
2015-10-01 16:20:22 +0330

    Include missing files

---
+++ Simila.Core.Tests/SimilaStringTests/PluggableSimilaTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using Simila.Core.Levenstein;
+
+namespace Simila.Core.Tests.SimilaStringTests
+{
+    [TestFixture]
+    public class PluggableSimilaTests
+    {
+        [Test]
+        [ExpectedException(typeof(ResolutionFailedException))]
+        public void Simila_ShouldThrowException_WithNoConfig()
+        {
+            var simila = new Simila(SimilaType.Manual, null);
+
+            Assert.IsTrue(simila.AreSimilar("Cat", "Kat"));
+        }
+
+        [Test]
+        public void Simila_ShouldWork_WithOverridingConfigs()
+        {
+            var simila = new Simila();
+
+            Assert.IsTrue(simila.AreSimilar("Cat", "Kat"));
+
+            // Overriding Word similarity resolver method.
+            simila.Resolver.RegisterType<ISimilarityResolver<Word>, MistakeBasedSimilarityResolver<Word>>();
+
+            Assert.IsFalse(simila.AreSimilar("Cat", "Kat"));
+            Assert.IsTrue(simila.AreSimilar("color", "colour"));
+        }
+    }
+}
---
+++ Simila.Core/Resovlers/MistakeBasedResolver/Mistakes/BuiltInMistakeRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Simila.Core.Levenstein;
+
+namespace Simila.Core
+{
+    public class BuiltInCharacterMistakeRepository : IMistakeRepository<char>
+    {
+        public List<Mistake<char>> GetMistakes()
+        {
+            return new List<Mistake<char>>()
+            {
+                new Mistake<char>('c', 'k', .50f),
+                new Mistake<char>('o', 'u', .50f),
+                new Mistake<char>('l', 'i', .50f),
+                new Mistake<char>('i', 'y', .70f),
+                new Mistake<char>('v', 'w', .50f),
+                new Mistake<char>('z', 's', .40f),
+                new Mistake<char>('s', 'c', .50f),
+                new Mistake<char>('c', 'k', .50f),
+                new Mistake<char>('n', 'm', .50f),
+
+                
+                new Mistake<char>('a', 'e', .30f),
+
+            };
+        }
+    }
+
+    public class BuiltInWordMistakeRepository : IMistakeRepository<Word>
+    {
+        public List<Mistake<Word>> GetMistakes()
+        {
+            return new List<Mistake<Word>>()
+            {
+                new Mistake<Word>("color", "colour", 0.90f),
+            };
+        }
+    }
+}
