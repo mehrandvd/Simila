589de38d636dac4d2a19f3a5bd7993526ad396eb
ysmoradi <ysmoradi@outlook.com>
2019-10-24 20:49:54 +0330

    Updated to new csproj format

--- Simila.Studio/MainForm.cs
+++ Simila.Studio/MainForm.cs
@@ -1,20 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml;
-using Microsoft.Practices.Unity;
-using Simila.Core;
-using Simila.Core.Levenstein;
-using Simila.Studio;
 
 namespace Simila.Studio
 {
